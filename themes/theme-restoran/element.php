e1925b0119cc655f2ab2dc7dd741b2ae1509c7c8
unknown <anwarrivaldi@gmail.com>
2025-10-16 05:17:55 +0700

    adjustment

---
+++ themes/theme-restoran/element.php
@@ -0,0 +1,12 @@
+<?php
+
+// this pages will be the master page which list all available elements accross all themes
+
+return [
+    'home' => [
+        'hero' => ['tagline', 'button_label', 'spinning_text'],
+        'services' => ['items']
+        // other elements
+    ],
+    // other pages
+];
