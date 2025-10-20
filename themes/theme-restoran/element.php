5d20fc00e243a8618677ff82e044c21afba2f9dd
MRivaldiAP <113491822+MRivaldiAP@users.noreply.github.com>
2025-10-20 19:23:40 +0700

    Align restoran contact nav styling

--- themes/theme-restoran/element.php
+++ themes/theme-restoran/element.php
@@ -38,6 +38,32 @@ return [
             'contact.map',
         ],
     ],
+    'contact' => [
+        'hero' => [
+            'hero.visible',
+            'hero.mask',
+            'hero.background',
+            'hero.heading',
+            'hero.description',
+        ],
+        'details' => [
+            'details.visible',
+            'details.heading',
+            'details.description',
+            'details.items',
+        ],
+        'social' => [
+            'social.visible',
+            'social.heading',
+            'social.description',
+            'social.items',
+        ],
+        'map' => [
+            'map.visible',
+            'map.heading',
+            'map.embed',
+        ],
+    ],
     'product' => [
         'hero' => [
             'hero.visible',
@@ -176,6 +202,7 @@ return [
             'navigation.link.about',
             'navigation.link.products',
             'navigation.link.gallery',
+            'navigation.link.contact',
             'navigation.link.articles',
             'navigation.link.article-detail',
             'navigation.link.orders',
