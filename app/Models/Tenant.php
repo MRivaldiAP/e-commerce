0037a4c73f0bfb9d02cb8ac35b61d8ef62b80080
MRivaldiAP <113491822+MRivaldiAP@users.noreply.github.com>
2025-10-31 05:24:55 +0700

    Revert "Add central tenant management UI and routing"

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -10,28 +10,4 @@ use Stancl\Tenancy\Database\Concerns\HasDomains;
 class Tenant extends BaseTenant implements TenantWithDatabase
 {
     use HasDatabase, HasDomains;
-
-    protected $fillable = [
-        'id',
-        'data',
-        'name',
-    ];
-
-    public function getNameAttribute(): ?string
-    {
-        return $this->data['name'] ?? null;
-    }
-
-    public function setNameAttribute(string $value): void
-    {
-        $data = $this->data ?? [];
-        $data['name'] = $value;
-
-        $this->data = $data;
-    }
-
-    public function getPrimaryDomainAttribute(): ?string
-    {
-        return $this->domains->first()->domain ?? null;
-    }
 }
